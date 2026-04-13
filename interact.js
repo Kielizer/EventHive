5b02e87a82dd759dbc54108a77e4f063fcc475a9
Kielizer <paolocaling020@gmail.com>
2026-04-13 14:13:08 +0800

    Your commit message here

---
+++ interact.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const menuToggle = document.getElementById("menu-toggle");
+    const sideMenu = document.getElementById("side-menu");
+    const overlay = document.getElementById("overlay");
+
+    // TOGGLE MENU
+    menuToggle.addEventListener("click", () => {
+        sideMenu.classList.toggle("active");
+        overlay.classList.toggle("active");
+    });
+
+    // CLOSE WHEN CLICK OUTSIDE
+    overlay.addEventListener("click", () => {
+        sideMenu.classList.remove("active");
+        overlay.classList.remove("active");
+    });
+
+});
